9ae3253bbef84366bc8ead96414bc3d5c7ee12d1
unknown <brunoduarte40@gmail.com>
2021-06-23 20:11:49 -0300

    correcao dados de entrada para bolas mais sorteadas

--- src/todas_possibilidades_bolas_mais_sorteadas.py
+++ src/todas_possibilidades_bolas_mais_sorteadas.py
@@ -1,24 +1,10 @@
 import pandas as pd
 import itertools
-import numpy as np
-from random import randrange, uniform
-bolas_mais_sorteadas_renomeada = pd.read_csv(r'C:\lotoFacilLast100\dados\LOTOFACIL.csv', sep=';')
-bolas_mais_sorteadas_renomeada
 
-df = bolas_mais_sorteadas_renomeada
-df_tds_possib = pd.DataFrame([e for e in itertools.product(df.B1,
-                                                           df.B2,
-                                                           df.B3,
-                                                           df.B4,
-                                                           df.B5,
-                                                           df.B6,
-                                                           df.B7,
-                                                           df.B8,
-                                                           df.B9,
-                                                           df.B10,
-                                                           df.B11,
-                                                           df.B12,
-                                                           df.B13,
-                                                           df.B14,
-                                                           df.B15)], columns=df.columns)
+df = pd.read_csv(r'C:\lotoFacilLast100\dados\bolas_mais_sorteadas.csv', sep=';')
+
+df_tds_possib = pd.DataFrame([e for e in itertools.product(df.B1,df.B2,df.B3,df.B4,df.B5,
+                                                           df.B6,df.B7,df.B8,df.B9,df.B10,
+                                                           df.B11,df.B12,df.B13,df.B14,df.B15)],
+                             columns=df.columns)
 df_tds_possib.to_csv(r'C:\lotoFacilLast100\dados\todas_possibilidades_bolas_mais_sorteadas.csv', sep=';', index = False)
