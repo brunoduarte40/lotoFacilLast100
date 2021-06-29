519cd7efac0fb67125d34f7c1eb628cce4747ced
unknown <brunoduarte40@gmail.com>
2021-06-28 21:03:41 -0300

    inclusao de novas funcionalidades com exe e atualizacao de dados

--- output/LotoFacil.bat
+++ output/LotoFacil.bat
@@ -1,3 +1,11 @@
 start C:\lotoFacilLast100\output\extrair_ultimo_sorteio\extrair_ultimo_sorteio.exe
-timeout 10 /nobreak
+timeout 170 /nobreak
 start C:\lotoFacilLast100\output\seleciona_ult_100_sorteios\seleciona_ult_100_sorteios.exe
+start C:\lotoFacilLast100\output\grafico_tendencia_por_bola\grafico_tendencia_por_bola.exe
+timeout 10 /nobreak
+start C:\lotoFacilLast100\output\ocorrencias\ocorrencias.exe
+timeout 10 /nobreak
+start C:\lotoFacilLast100\output\organiza_ocorrencias\organiza_ocorrencias.exe
+start C:\lotoFacilLast100\output\todas_possibilidades_bolas_mais_sorteadas\todas_possibilidades_bolas_mais_sorteadas.exe
+timeout 10 /nobreak
+start C:\lotoFacilLast100\output\40_possibilidades_aleatorias\40_possibilidades_aleatorias.exe
